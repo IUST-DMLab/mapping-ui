d096120917826dfffcdbe1a6dc0180ac16ddd214
ehsan <ehsan.shahshahani@gmail.com>
2017-03-30 00:26:01 +0430

    برای صفحه property-mapping : ۱. افزودن ستون TupleCount به فهرست ۲. نمایش تعداد کل آیتم‌های فهرست ۳. اصلاح جستجو ۴. نمایش loading هنگام ارتباط با سرور

--- scripts/app/controller/main.js
+++ scripts/app/controller/main.js
@@ -36,104 +36,106 @@ app.controller('MainController', function ($scope, $timeout, RestService) {
 
         RestService.translationTranslate(name, edit.faLabel,
             edit.faOtherLabels, edit.note, edit.approved).success(function (data) {
-           $scope.data.data[index].faLabel = edit.faLabel;
-           $scope.data.data[index].faOtherLabels = edit.faOtherLabels;
-           $scope.data.data[index].note = edit.note;
-           $scope.data.data[index].approved = edit.approved;
-        });
+                $scope.data.data[index].faLabel = edit.faLabel;
+                $scope.data.data[index].faOtherLabels = edit.faOtherLabels;
+                $scope.data.data[index].note = edit.note;
+                $scope.data.data[index].approved = edit.approved;
+            });
     }
 });
 
 
 app.controller('TemplateMappingController', function ($scope, $timeout, RestService) {
-   $scope.go = go;
-   $scope.save = save;
-   $scope.revert = revert;
+    $scope.go = go;
+    $scope.save = save;
+    $scope.revert = revert;
 
-   //$scope.loadClasses = function (query) {
-   //    return RestService.ontologyClassSearch(0, 100, query);
-   //};
+    //$scope.loadClasses = function (query) {
+    //    return RestService.ontologyClassSearch(0, 100, query);
+    //};
 
-   $scope.autoCompleteOptions = {
-      minimumChars: 1,
-      data: function (term) {
-         return RestService.ontologyClassSearch(0, 100, term);
-      }
-   };
+    $scope.autoCompleteOptions = {
+        minimumChars: 1,
+        data: function (term) {
+            return RestService.ontologyClassSearch(0, 100, term);
+        }
+    };
 
-   $timeout(function () {
-      go(0);
-   }, 500);
+    $timeout(function () {
+        go(0);
+    }, 500);
 
-   function go(page, search, approved, hasFarsi) {
-      $scope.filter = {
-         page: page,
-         search: search,
-         approved: approved,
-         hasFarsi: hasFarsi
-      };
-      RestService.templateMappingSearch($scope.filter.page, 40, $scope.filter.search, true, $scope.filter.approved, $scope.filter.hasFarsi)
-          .success(function (data) {
-             $scope.data = data;
-             $scope.pages = [];
-             for (var i = 0; i < data.pageCount; i++) $scope.pages.push(i + 1);
-          });
-   }
+    function go(page, search, approved, hasFarsi) {
+        $scope.filter = {
+            page: page,
+            search: search,
+            approved: approved,
+            hasFarsi: hasFarsi
+        };
+        RestService.templateMappingSearch($scope.filter.page, 40, $scope.filter.search, true, $scope.filter.approved, $scope.filter.hasFarsi)
+            .success(function (data) {
+                $scope.data = data;
+                $scope.pages = [];
+                for (var i = 0; i < data.pageCount; i++) $scope.pages.push(i + 1);
+            });
+    }
 
-   function save(index, item) {
-      RestService.templateMappingSave(item).success(function (data) {
-         $scope.data.data[index].ontologyClass = data.ontologyClass;
-         $scope.data.data[index].approved = data.approved;
-      });
-   }
+    function save(index, item) {
+        RestService.templateMappingSave(item).success(function (data) {
+            $scope.data.data[index].ontologyClass = data.ontologyClass;
+            $scope.data.data[index].approved = data.approved;
+        });
+    }
 
-   function revert(index) {
-      $scope.data.data[index] = $scope.copy.data[index];
-   }
+    function revert(index) {
+        $scope.data.data[index] = $scope.copy.data[index];
+    }
 });
 
 
 app.controller('PropertyMappingController', function ($scope, $timeout, RestService) {
-   $scope.go = go;
-   $scope.save = save;
-   $scope.revert = revert;
+    $scope.go = go;
+    $scope.save = save;
+    $scope.revert = revert;
+    $scope.filter = {};
 
-   $scope.autoCompleteOptions = {
-      minimumChars: 1,
-      data: function (term) {
-         return RestService.ontologyPropertyNameSearch(0, 100, term);
-      }
-   };
+    $scope.clear = function(){
+        $scope.filter = {};
+        $scope.go(0);
+    };
 
-   $scope.statusList = ['Approved', 'NearlyApproved', 'NotApproved', 'Multiple', 'Translated', 'NotMapped'];
+    $scope.autoCompleteOptions = {
+        minimumChars: 1,
+        data: function (term) {
+            return RestService.ontologyPropertyNameSearch(0, 100, term);
+        }
+    };
 
-   $timeout(function () {
-      go(0);
-   }, 500);
+    $scope.statusList = ['Approved', 'NearlyApproved', 'NotApproved', 'Multiple', 'Translated', 'NotMapped'];
 
-   function go(page, search, approved, hasFarsi) {
-      $scope.filter = {
-         page: page,
-         search: search,
-         approved: approved,
-         hasFarsi: hasFarsi
-      };
-      RestService.propertyMappingSearch($scope.filter.page, 40, $scope.filter.search, true, $scope.filter.approved, $scope.filter.hasFarsi)
-          .success(function (data) {
-             $scope.copy = angular.copy(data);
-             $scope.data = data;
-             $scope.data.pageNo = $scope.data.page + 1;
-          });
-   }
+    $timeout(function () {
+        go(0);
+    }, 500);
 
-   function save(index, item) {
-      RestService.propertyMappingSave(item).success(function (data) {
-         $.extend($scope.data.data[index], data);
-      });
-   }
+    function go(page) {
 
-   function revert(index) {
-      $scope.data.data[index] = $scope.copy.data[index];
-   }
+        RestService.propertyMappingSearch(page, 40, $scope.filter.templateName, $scope.filter.ontologyClass, $scope.filter.templateProperty,
+            $scope.filter.ontologyProperty, true, $scope.filter.approved, $scope.filter.status)
+            .success(function (data) {
+                $scope.copy = angular.copy(data);
+                $scope.data = data;
+                $scope.data.pageNo = $scope.data.page + 1;
+            });
+    }
+
+    function save(index, item) {
+        RestService.propertyMappingSave(item).success(function (data) {
+            $.extend($scope.data.data[index], data);
+        });
+    }
+
+    function revert(index) {
+        $scope.data.data[index] = $scope.copy.data[index];
+    }
 });
 
